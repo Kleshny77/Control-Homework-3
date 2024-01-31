55ffdd08b9fd0166b45946bdf161c190bcc9b4db
Артём <144159993+Kleshny77@users.noreply.github.com>
2024-01-31 23:05:31 +0300

    Add files via upload

---
+++ Control homework November2/ClassLibary/CsvProcessing.cs
@@ -0,0 +1,285 @@
+﻿using System.IO;
+
+namespace ClassLibary;
+/// <summary>
+/// Cодержит методы для чтения и записи данных в csv файл.
+/// </summary>
+public class CsvProcessing
+{
+    private static string fPath;
+    /// <summary>
+    /// Получение пути к файлу.
+    /// </summary>
+    /// <param name="path">Абсолютный путь к файлу.</param>
+    public static void GetPath(string path)
+    {
+        fPath = path;
+    }
+    /// <summary>
+    /// Считывает файл в массив строк. Если файла не существует, выбрасывает исключение.
+    /// </summary>
+    /// <returns>Данные таблицы в виде массива строк.</returns>
+    /// <exception cref="ArgumentNullException">Файла не существует.</exception>
+    public static string[] Read()
+    {
+        // Проверка существования файла.
+        if (!File.Exists(fPath))
+        {
+            // Выбрасывание исключения.
+            throw new ArgumentNullException("Файл не найден.");
+        }
+        // Считывание файла.
+        string[] table = File.ReadAllLines(fPath);
+        return table;
+    }
+    /// <summary>
+    /// Метод записывает данные в новый файл.
+    /// </summary>
+    /// <param name="data">Данные, которые надо записать в файл.</param>
+    /// <param name="nPath">Путь к новому файлу.</param>
+    public static void Write(string data, string nPath)
+    {
+        try
+        {
+            // Проверка существования файла.
+            if (File.Exists(nPath))
+            {
+                // Дописываем данные в конец файла.
+                File.AppendAllText(nPath, data);
+            }
+            else
+            {
+                // Если файл не существует, создаем новый файл и записываем данные.
+                File.WriteAllText(nPath, data);
+            }
+        }
+        catch (UnauthorizedAccessException)
+        {
+            // Ошибка доступа к файлу из-за недостаточных прав доступа.
+            Console.WriteLine("Ошибка доступа - недостаточно прав доступа к файлу.");
+        }
+        catch (PathTooLongException)
+        {
+            // Ошибка из-за слишком длинного пути к файлу.
+            Console.WriteLine("Ошибка - слишком длинный путь к файлу.");
+        }
+        catch (DirectoryNotFoundException)
+        {
+            // Ошибка, если директория пути файла не существует.
+            Console.WriteLine("Ошибка - директория пути файла не существует.");
+        }
+        catch (IOException)
+        {
+            // Ошибка ввода-вывода.
+            Console.WriteLine("Ошибка ввода-вывода при работе с файлом.");
+        }
+        catch (NotSupportedException)
+        {
+            // Ошибка, если операция не поддерживается.
+            Console.WriteLine("Ошибка - операция не поддерживается.");
+        }
+        catch (Exception ex)
+        {
+            // Любая другая необработанная ошибка.
+            Console.WriteLine("Ошибка записи данных в файл: " + ex.Message);
+        }
+    }
+    /// <summary>
+    /// Метод записывает новые данные в уже существующий файл, стирая старые данные в нём.
+    /// </summary>
+    /// <param name="data">Данные, которые надо перезаписать в старый файл.</param>
+    /// <param name="fPath">Путь к существующему файлу</param>
+    public static void Write(string[] data, string fPath)
+    {
+        try
+        {
+            File.WriteAllLines(fPath, data);
+        }
+        catch (UnauthorizedAccessException)
+        {
+            // Ошибка доступа к файлу из-за недостаточных прав доступа.
+            Console.WriteLine("Ошибка доступа - недостаточно прав доступа к файлу.");
+        }
+        catch (PathTooLongException)
+        {
+            // Ошибка из-за слишком длинного пути к файлу.
+            Console.WriteLine("Ошибка - слишком длинный путь к файлу.");
+        }
+        catch (DirectoryNotFoundException)
+        {
+            // Ошибка, если директория пути файла не существует.
+            Console.WriteLine("Ошибка - директория пути файла не существует.");
+        }
+        catch (IOException)
+        {
+            // Ошибка ввода-вывода.
+            Console.WriteLine("Ошибка ввода-вывода при работе с файлом.");
+        }
+        catch (NotSupportedException)
+        {
+            // Ошибка, если операция не поддерживается.
+            Console.WriteLine("Ошибка - операция не поддерживается.");
+        }
+        catch (Exception ex)
+        {
+            // Любая другая необработанная ошибка.
+            Console.WriteLine("Ошибка записи данных в файл: " + ex.Message);
+        }
+    }
+}
+/// <summary>
+/// Содержит методы для работы с данными, полученными из файла.
+/// </summary>
+public class DataProcessing
+{
+    /// <summary>
+    /// Делает выборку по определенным строкам определенных столбцов.
+    /// </summary>
+    public static string[] Selection(int choice)
+    {
+        // Получение значения полей от пользователя
+        Console.Write("Значение поля Adm Area: ");
+        string admArea = Console.ReadLine();
+        Console.Write("Значение поля Carcapacity: ");
+        string carCapacity = Console.ReadLine();
+        Console.Write("Значение поля District: ");
+        string district = Console.ReadLine();
+        Console.Write("Значение поля Mode: ");
+        string mode = Console.ReadLine();
+        string[] table = new string[0];
+        try
+        {
+            // Получение таблицы из файла.
+            table = CsvProcessing.Read();
+            string[] result = new string[0];
+            for (int i = 0; i < table.Length; i++)
+            {
+                // Разбиваем строку таблицы на поля.
+                string[] rowFields = table[i].Split(new string[] { "\";\"", "\";", "\"", ";" }, StringSplitOptions.None);
+                bool flag = false;
+                switch (choice)
+                {
+                    case 1:
+                        flag = rowFields[4] == admArea;
+                        break;
+                    case 2:
+                        flag = rowFields[10] == carCapacity;
+                        break;
+                    case 3:
+                        flag = rowFields[5] == district && rowFields[11] == mode;
+                        break;
+                    case 4:
+                        flag = rowFields[4] == admArea && rowFields[10] == carCapacity && rowFields[5] == district && rowFields[11] == mode;
+                        break;
+                }
+                // Проверяем соответствие каждого поля критерию поиска.
+                if (flag)
+                {
+                    Array.Resize(ref result, result.Length + 1);
+                    result[result.Length - 1] = table[i];
+                }
+            }
+            // Вывод результата на экран.
+            if (result.Length > 0)
+            {
+                foreach (string row in result)
+                {
+                    string[] rowFields = row.Split(new string[] { "\";\"", " "}, StringSplitOptions.None);
+                    for (int i = 0; i < rowFields.Length; i++)
+                    {
+                        string field = rowFields[i].Trim('"');
+                        Console.Write(field.PadRight(5));
+                    }
+                    Console.WriteLine();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Результат выборки пуст.");
+            }
+        }
+        catch (ArgumentNullException ex)
+        {
+            Console.WriteLine("Ошибка при чтении файла: " + ex.Message);
+        }
+        return table;
+    }
+    /// <summary>
+    /// Cортировка по значениям CarCapacity по уменьшению ёмкости.
+    /// </summary>
+    public static string[] SortedLow()
+    {
+        // Получение таблицы из файла.
+        string[] table = CsvProcessing.Read();
+        int columnIndex = 9;
+        ///<summary>
+        /// Метод для корректной сортировки значений.
+        /// </summary>
+        /// <returns>
+        /// 0 - если значения равны либо меньшее из двух значений.
+        /// </returns>
+        int CompareByColumn(string x, string y)
+        {
+            string[] xFields = x.Split(',');
+            string[] yFields = y.Split(',');
+
+            if (xFields.Length > columnIndex && yFields.Length > columnIndex)
+            {
+                int xValue = int.Parse(xFields[columnIndex]);
+                int yValue = int.Parse(yFields[columnIndex]);
+
+                // Сравнение в обратном порядке (убывание).
+                return yValue.CompareTo(xValue);
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        Array.Sort(table, CompareByColumn);
+        // Вывод таблицы в консоль.
+        foreach (string row in table)
+        {
+            string[] rowFields = row.Split(';');
+            foreach (string field in rowFields)
+            {
+                Console.Write(field + "\t");
+            }
+            Console.WriteLine();
+        }
+        return table;
+    }
+    /// <summary>
+    /// Cортировка по значениям CarCapacity по увеличению ёмкости.
+    /// </summary>
+    public static string[] SortedUp()
+    {
+        // Получение таблицы из файла.
+        string[] table = CsvProcessing.Read();
+        int columnIndex = 10;
+        Array.Sort(table, (x, y) =>
+        {
+            string[] xColumns = x.Split(new string[] { "\";\"", " " }, StringSplitOptions.None);
+            string[] yColumns = y.Split(new string[] { "\";\"", " " }, StringSplitOptions.None);
+            if (xColumns.Length > columnIndex && yColumns.Length > columnIndex)
+            {
+                return string.Compare(xColumns[columnIndex],yColumns[columnIndex]);
+            }
+            else
+            {
+                return 0;
+            }
+        });
+        // Вывод отсортированных данных на экран.
+        foreach (string row in table)
+        {
+            string[] rowFields = row.Split(';');
+            foreach (string field in rowFields)
+            {
+                Console.Write(field + "\t");
+            }
+            Console.WriteLine();
+        }
+        return table;
+    }
+}
---
+++ Control homework November2/Console Application/Program.cs
@@ -0,0 +1,172 @@
+﻿namespace Console_Application;
+class Program
+{
+    /// <summary>
+    /// Метод для считывания с консоли переменной типа int м проверки этой переменной на ограничения. Если ограничений нет, то происходит такая проверка: -бесконечность < введенное число < +бесконечность.
+    /// </summary>
+    /// <param name="name">Имя переменной, которую надо считать. По умолчанию имени нет.</param>
+    /// <param name="lowerBound">Нижняя граница ограничения. По умолчанию равна -бесконечности.</param>
+    /// <param name="upperBound">Верхняя граница ограничения. По умолчанию равна +бесконечности.</param>
+    /// <param name="lowerSing">Строгость знака нижней границы ограничения. По умолчанию строгий знак ("<").</param>
+    /// <param name="upperSing">Строгость знака верхней границы ограничения. По умолчанию строгий знак ("<").</param>
+    /// <returns>Переменная типа int, которую ввёл пользователь.</returns>
+    public static int InputInt(string name = "", double lowerBound = double.NegativeInfinity, double upperBound = double.PositiveInfinity, string lowerSing = "<", string upperSing = "<")
+    {
+        bool flag;
+        // Временная переменная для проверки на корректность ввода данных.
+        int result;
+        // Возвращаемая переменная.
+        do
+        {
+            Console.Write($"{lowerBound} {lowerSing} {name} {upperSing} {upperBound}. {name} = ");
+            // Предупреждение пользователя об ограничениях на переменную, которую надо ввести.
+            if (!int.TryParse(Console.ReadLine(), out result))
+            {
+                Console.WriteLine("Некорректный ввод, повторите попытку");
+                flag = false;
+                // Срабатывает, если пользователь ввел любой символ, не содержащий цифр.
+            }
+            else
+            {
+                bool flag1 = true;
+                // Временная переменная создания проерки на ограничения. Проверяет нижнее ограничение.
+                bool flag2 = true;
+                // Временная переменная создания проерки на ограничения. Проверяет верхнее ограничение.
+                switch (lowerSing)
+                {
+                    case "<":
+                        flag1 = lowerBound < result;
+                        break;
+                    case "<=":
+                        flag1 = lowerBound <= result;
+                        break;
+                }
+                switch (upperSing)
+                {
+                    case "<":
+                        flag2 = result < upperBound;
+                        break;
+                    case "<=":
+                        flag2 = result <= upperBound;
+                        break;
+                }
+                if (flag1 && flag2)
+                {
+                    flag = true;
+                }
+                else
+                {
+                    Console.WriteLine($"Введённые значения не подходят под ограничения. {lowerBound} {lowerSing} {name} {upperSing} {upperBound}");
+                    flag = false;
+                }
+            }
+        }
+        while (!flag);
+        // Цикл повторяется до тех пор, пока пользователь не введет корректные значения.
+        return result;
+    }
+    ///<summary>
+    ///Этот метод запрашивает у пользователя абсолютный путь к файлу с csv-данными.
+    ///</summary>
+    ///<returns>
+    ///Переменная типа string, содержащая абсолютный путь к файлу с csv-данными.
+    ///</returns>
+    public static string AbsolutePath(string name)
+    {
+        string path;
+        Console.Write($"{name} = ");
+        path = Console.ReadLine();
+        return path;
+    }
+    /// <summary>
+    /// Этот метод считывает определенные данные с файла.
+    /// </summary>
+    /// <returns>
+    /// Значение типа bool для выхода из программы.
+    /// </returns>
+    /// <param name="path">
+    /// Абсолютный путь файла.
+    /// </param>
+    public static string[] GettingData(string path)
+    {
+        Console.BackgroundColor = ConsoleColor.Black;
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.WriteLine("Укажите номер пункта меню для запуска действия:");
+        Console.WriteLine("    1. Произвести выборку по значению AdmArea");
+        Console.WriteLine("    2. Произвести выборку по значению CarCapacity");
+        Console.WriteLine("    3. Произвести выборку по значению District и Mode");
+        Console.WriteLine("    4. Произвести выборку по значениям AdmArea, CarCapacity, District и Mode");
+        Console.WriteLine("    5. Отсортировать таблицу по значению CarCapacity (по увеличению ёмкости)");
+        Console.WriteLine("    6. Отсортировать таблицу по значению CarCapacity (по уменьшению ёмкости)");
+        Console.WriteLine("    7. Выйти из программы");
+        int choice = InputInt("Ваш выбор", lowerBound: 0, upperBound: 8);
+        Console.ResetColor();
+        string[] result = null;
+        switch (choice)
+        {
+            case 1:
+                // Реализация выборки по значению AdmArea.
+                result = ClassLibary.DataProcessing.Selection(1);
+                break;
+            case 2:
+                // Реализация выборки по значению CarCapacity.
+                result = ClassLibary.DataProcessing.Selection(2);
+                break;
+            case 3:
+                // Реализация выборки по значению District и Mode.
+                result = ClassLibary.DataProcessing.Selection(3);
+                break;
+            case 4:
+                // Реализация выборки по значению AdmArea, CarCapacity, District и Mode.
+                result = ClassLibary.DataProcessing.Selection(4);
+                break;
+            case 5:
+                // Реализация сортировки по значению CarCapacity (по увеличению ёмкости).
+                result = ClassLibary.DataProcessing.SortedUp();
+                break;
+            case 6:
+                // Реализация сортировки по значению CarCapacity (по уменьшению ёмкости).
+                result = ClassLibary.DataProcessing.SortedLow();
+                break;
+            case 7:
+                // Выход из программы.
+                result = ClassLibary.CsvProcessing.Read();
+                ClassLibary.CsvProcessing.Write(result, AbsolutePath("Новый абсолютный путь, чтобы перезаписать файл"));
+                Console.Write("Попрощайтесь с программой: ");
+                Console.ReadLine();
+                Console.WriteLine("До свидания!");
+                // Досрочный выход из программы.
+                Environment.Exit(0);
+                break;
+        }
+        return result;
+    }
+    /// <summary>
+    /// Метод рисует очень патриотичный флаг России. Если вы поставите оценку за это кдз < 5, это будет очень непатриотично! (приравненно к иноагенству)
+    /// </summary>
+    public static void RussianFlag()
+    {
+        Console.BackgroundColor = ConsoleColor.White;
+        Console.WriteLine("                                                                                                                                    ");
+        Console.BackgroundColor = ConsoleColor.DarkBlue;
+        Console.WriteLine("                                                                                                                                    ");
+        Console.BackgroundColor = ConsoleColor.Red;
+        Console.WriteLine("                                                                                                                                    ");
+        Console.ResetColor();
+    }
+    static void Main(string[] args)
+    {
+        RussianFlag();
+        string fPath = AbsolutePath("Абсолютный путь");
+        ClassLibary.CsvProcessing.GetPath(fPath);
+        ClassLibary.CsvProcessing.Read();
+        string[] result = GettingData(fPath);
+        string nPath = AbsolutePath("Новый абсолютный путь");
+        // Переписывание данных.
+        ClassLibary.CsvProcessing.Write(result, nPath);
+        // Перекрашивание консоли в черный цвет.
+        Console.BackgroundColor = ConsoleColor.Black;
+        Console.Write("Попрощайтесь с программой: ");
+        Console.ReadLine();
+    }   
+}
